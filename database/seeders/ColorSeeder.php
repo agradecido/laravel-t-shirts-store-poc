e4f6025b55e042c794a5127db91f2ded22b08662
Javi Sierra <agradecido@gmail.com>
2024-01-22 20:21:30 +0100

    feat: create Color model with seeder and migration

---
+++ database/seeders/ColorSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ColorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $colors = [
+            [
+                'slug' => 'red',
+                'name' => 'Red',
+                'RGB' => 'FF0000',
+            ],
+            [
+                'slug' => 'green',
+                'name' => 'Green',
+                'RGB' => '00FF00',
+            ],
+            [
+                'slug' => 'blue',
+                'name' => 'Blue',
+                'RGB' => '0000FF',
+            ],
+            [
+                'slug' => 'black',
+                'name' => 'Black',
+                'RGB' => '000000',
+            ],
+            [
+                'slug' => 'white',
+                'name' => 'White',
+                'RGB' => 'FFFFFF',
+            ],
+        ];
+
+        foreach ($colors as $color) {
+            \App\Models\Color::create($color);
+        }
+    }
+}
